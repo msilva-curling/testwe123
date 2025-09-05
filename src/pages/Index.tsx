27761ea764b0c004b83716ad2df00ffe9f9bc734
Skip <skip@usecurling.com>
2025-09-05 14:40:59 +0000

    feat: Add multiple interactive squares to Index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,13 @@
 import { InteractiveSquare } from '@/components/InteractiveSquare'
 
 const Index = () => {
-  return <InteractiveSquare />
+  return (
+    <div className="flex flex-wrap items-center justify-center gap-8 p-4 md:gap-12 lg:gap-16">
+      <InteractiveSquare />
+      <InteractiveSquare />
+      <InteractiveSquare />
+    </div>
+  )
 }
 
 export default Index
